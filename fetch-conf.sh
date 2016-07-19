da956045e07c61f0701984739eb434ba48786998
Ossi Herrala <oherrala@gmail.com>
2016-07-19 22:36:42 +0300

    fetch-conf.sh: Script to download config from Mikrotik CRS devices

---
+++ fetch-conf.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "$0 <user>@<host> <switch name>"
+    exit 1
+fi
+
+SWITCH="$1"
+NAME="$2"
+CONF="${NAME}-$(date +%FT%T)"
+
+set -ex
+
+ssh ${SWITCH} system export verbose file=${CONF}-verbose
+ssh ${SWITCH} system export file=${CONF}-compact
+echo "get ${CONF}-verbose.rsc\nget ${CONF}-compact.rsc\n" | sftp -b - ${SWITCH}
