30a4d829884e829a90f3c40ac8b47fbf54d90100
Ossi Herrala <oherrala@gmail.com>
2016-07-31 21:55:10 +0300

    fetch-conf.sh: learned to fetch compact (non-verbose) config

--- fetch-conf.sh
+++ fetch-conf.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 set -e
 
-if [ -z $1 ]; then
-    echo "$0 <user>@<host>"
+# Usage:
+#   fetch_conf.sh [-s] <user>@<port>
+#     -s            = Fetch compact config instead of verbose one
+#     <user>@<port> = SSH <user> and <host> to connect to
+
+usage() {
+    echo "$0 [-s] <user>@<host>"
     exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
+fi
+
+if [ "-s" = "$1" ] && [ -z "$2" ]; then
+    usage
+fi
+
+if [ "-s" = "$1" ]; then
+    shift
+    CMD='export hide-sensitive file="$[system identity get name]"'
+else
+    CMD='export verbose hide-sensitive file="$[system identity get name]"'
 fi
 
 set -ex
 SWITCH="$1"
 
-ssh ${SWITCH} 'export verbose hide-sensitive file="$[system identity get name]"'
-echo 'get *.rsc\n' | sftp -b - ${SWITCH}
+ssh "${SWITCH}" "${CMD}" # shellcheck disable=SC2029
+printf 'get *.rsc\n' | sftp -b - "${SWITCH}"
